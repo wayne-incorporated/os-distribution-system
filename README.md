15d62ef872663c6dad2787cf46262a0343f14bcf
Jeun91 <dev@wayne-inc.com>
2019-06-28 17:44:06 +0900

    2019-06-28 Weekly Update

---
+++ README.md
@@ -0,0 +1,10 @@
+# OS Distribution System
+
+This project is to improve and open source code for OS Distribution System by Wayne Inc. It will be online since June 2019.
+
+This system enables the users to download and update the OS easily.
+
+There are 2 benefits for using this system.
+
+1. The OS provider can manage the distribution of OS in a more profitable way.
+2. It is easier for the users to download & install OS on the USB. 
--- wayneOSforWin/View/widgetselectversion.cpp
+++ wayneOSforWin/View/widgetselectversion.cpp
@@ -9,14 +9,6 @@
 WidgetSelectVersion::WidgetSelectVersion(QWidget *parent) :QWidget(parent), ui(new Ui::WidgetSelectVersion)
 {
     ui->setupUi(this);
-	/*ui->textEdit->setFixedSize((int)(windowsDpiScale()*1.5*ui->textEdit->width()), (int)(windowsDpiScale()*1.5*ui->textEdit->height()));
-	ui->textEdit->move(QPoint(ui->textEdit->x()*1.5*windowsDpiScale(), ui->textEdit->y()*1.5*windowsDpiScale()));
-	ui->verticalLayoutWidget->setFixedSize((int)(windowsDpiScale()*1.5*ui->verticalLayoutWidget->width()), (int)(windowsDpiScale()*1.5*ui->verticalLayoutWidget->height()));
-	ui->verticalLayoutWidget->move(QPoint(ui->verticalLayoutWidget->x()*1.5*windowsDpiScale(), ui->verticalLayoutWidget->y()*1.5*windowsDpiScale()));
-	ui->btnPrev->setFixedSize((int)(windowsDpiScale()*1.5*ui->btnPrev->width()), (int)(windowsDpiScale()*1.5*ui->btnPrev->height()));
-	ui->btnPrev->move(QPoint(ui->btnPrev->x()*1.5*windowsDpiScale(), ui->btnPrev->y()*1.5*windowsDpiScale()));
-	ui->btnNext->setFixedSize((int)(windowsDpiScale()*1.5*ui->btnNext->width()), (int)(windowsDpiScale()*1.5*ui->btnNext->height()));
-	ui->btnNext->move(QPoint(ui->btnNext->x()*1.5*windowsDpiScale(), ui->btnNext->y()*1.5*windowsDpiScale()));*/
     ui->radioFree->click(); // modified by LEE jeun jeun@wayne-inc.com
 }
 
